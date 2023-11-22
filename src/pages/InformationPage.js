d088f91cc26e7e2b3121620995a4c16f98c95251
777Mino777 <petctusweet@naver.com>
2023-11-22 19:12:29 +0900

    Update InformationPage.js
    information 완성
    테이블 마저 구현 완료

--- src/pages/InformationPage.js
+++ src/pages/InformationPage.js
@@ -14,15 +14,20 @@ const InformationPage = () => {
   //   return n + '0대'
   // }
 
-  const testTable = [
-    {
-      gender: "여자", age : "30대", bmi : "정상", exerciseStep: "중간", exerciseType: "달리기"
-    },
-  ];
-
+  // const testTable = [
+  //   {
+  //     gender: "여자", age : "30대", bmi : "정상", exerciseStep: "중간", exerciseType: "달리기"
+  //   },
+  // ];
+  
+  const heightInMeters = height / 100;
+  const checkBmi = height && weight ? (weight / (heightInMeters * heightInMeters)).toFixed(2) : " ";
+  
   const calcBmi = (height, weight) => {
-    const heightInMeters = height / 100;
-    const checkBmi = weight / (heightInMeters * heightInMeters);
+    
+
+    if (height === '' || weight === '')
+      return '';
 
     if (checkBmi >= 35.0)
       return "3단계비만"
@@ -31,28 +36,31 @@ const InformationPage = () => {
     else if (checkBmi <= 29.9 && checkBmi >= 25.0)
       return "1단계비만"
     else if (checkBmi <= 24.9 && checkBmi >= 23.0)
-      return "비만전단계"
+      return "과체중"
     else if (checkBmi <= 22.9 && checkBmi >= 18.5)
       return "정상"
     else
       return "저체중"
   }
 
+  const bmi = calcBmi(height, weight);
   const handleView = async () => {
 
-    const bmi = calcBmi(height, weight);
 
     // const testage = calcAge(age);
     // const testbmi = '정상';
     console.log(age)
 
-    setUserInfo(testTable);
+    // setUserInfo();
 
-    // const url = `http://10.125.121.216:8080/api/vitallog/information?age=${age}&bmi=${bmi}&gender=${gender}`
+    const url = `http://10.125.121.216:8080/api/vitallog/information?age=${age}&bmi=${bmi}&gender=${gender}`
 
-    // fetch(url)
-    //   .then((resp) => resp.json())
-    //   .then((data) => console.log(data));
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {setUserInfo(data);
+      })
+      .catch((error) => console.error('Error', error));
+    
    
     /*
     try {
@@ -73,18 +81,20 @@ const InformationPage = () => {
         <header className="p-2 sm:p-6">
           <VlogNav />
         </header>
-        <h1 className="mx-16 pb-8 pt-16 text-gray-400 font-bold">선택하지 않은 경우 모든 옵션을 포함하는 정보를 볼 수 있습니다.</h1>
+        <h1 className="mx-16 pb-4 pt-10 text-gray-400 font-bold">선택하지 않은 경우 모든 옵션을 포함하는 정보를 볼 수 있습니다.</h1>
         <div className="form flex justify-center pb-20">
           <table className="flex justify-around">
             <thead className="px-10 space-x-32">
-              <select className='border-4 font-bold w-25 px-20 py-6 text-2xl rounded-2xl border-black transition duration-300 hover:border-custom-blue' value={gender} onChange={(e) => setGender(e.target.value)}>
+              <select className='border-4 font-bold w-25 px-14 py-6 text-2xl rounded-2xl border-black transition duration-300 hover:border-custom-blue' value={gender} onChange={(e) => setGender(e.target.value)}>
                 <option value="" className='hidden'>성별</option>
+                <option value="">선택없음</option>
                 <option value="M">남자</option>
                 <option value="F">여자</option>
               </select>
               <select className='border-4 font-bold w-25 px-12 py-6 text-2xl rounded-2xl border-black transition duration-300 hover:border-custom-blue'
                 value={age} onChange={(e) => setAge(e.target.value)}>
                 <option value="" className='hidden'>나이</option>
+                <option value="">선택없음</option>
                 <option value="10대" className='text-start'>10대</option>
                 <option value="20대" className='text-start'>20대</option>
                 <option value="30대" className='text-start'>30대</option>
@@ -111,13 +121,13 @@ const InformationPage = () => {
             </thead>
           </table>
         </div>
-        <div className='mx-14 flex pb-10 text-3xl justify-start space-x-80'>
-          <section className='border'>성별 : {}</section>
-          <section className='border'>나이 : {}</section>
-          <section className='border'>BMI : {}</section>
+        <div id="ulalala" className='mx-14 flex pb-4 text-3xl justify-start space-x-16'>
+          <section className='border-custom-blue font-bold pl-3 py-3 border-4 rounded-xl w-52'>성별 : {gender}</section>
+          <section className='border-custom-blue font-bold pl-3 py-3 border-4 rounded-xl w-72'>나이 : {age}</section>
+          <section className='border-custom-blue font-bold pl-3 py-3 border-4 rounded-xl w-96'>BMI :  {checkBmi} {checkBmi && bmi ? '/' : ''} {bmi}</section>
         </div>
-        <table className="border border-black text-3xl ml-14 mr-14 text-center">
-          <thead>
+        <table className="border-4 border-black text-3xl ml-14 mr-14 text-center mb-24">
+          <thead className='bg-sky-300'>
             <tr>
               <th className='border'>성별</th>
               <th className='border'>나이</th>
@@ -127,12 +137,12 @@ const InformationPage = () => {
             </tr>
             {/* <tr>&nbsp;</tr> */}
           </thead>
-          <tbody>
+          <tbody className='bg-sky-50'>
             {userInfo.map((user, index) => (
               <tr key={index}>
+                <td className='border'>{user.gender}</td>
                 <td className='border'>{user.age}</td>
                 <td className='border'>{user.bmi}</td>
-                <td className='border'>{user.gender}</td>
                 <td className='border'>{user.exerciseStep}</td>
                 <td className='border'>{user.exerciseType}</td>
               </tr>
