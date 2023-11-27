adfa702eaaef45582d711b8a123055da6baec497
777Mino777 <petctusweet@naver.com>
2023-11-27 11:31:22 +0900

    tailwindcss 업데이트
    폰트 추가 및
    게시판 글작성시 플레이스홀더
    센터 배치 코드

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,29 @@ module.exports = {
     extend: {
       colors: {
         'custom-blue' : '#0085C9',
-      },
+        },
       backgroundImage: theme => ({
         'custom-gradient': 'linear-gradient(180deg, #0085C9 0%, #FFF 100%)',
-      })
+      }),
+
+      fontFamily : {
+        'omyu_pretty' : ['omyu_pretty', 'sans-serif'],
+      }
     },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    function({ addUtilities }) {
+      const newUtilities = {
+        '.placeholder-center' : {
+          '&::placeholder' : {
+            textAlign : 'center',
+            verticalAlign : 'middle',
+          },
+        },
+      }
+      addUtilities(newUtilities, ['responsive', 'hover'])
+    }
+  ],
 };
 
