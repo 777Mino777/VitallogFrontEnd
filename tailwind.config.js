1a9a08286169e99e74778886a32c73e536c905f4
777Mino777 <petctusweet@naver.com>
2023-11-30 18:14:29 +0900

    tailwindcss 색상 추가
    information 컬러

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,13 @@ module.exports = {
     extend: {
       colors: {
         'custom-blue' : '#0085C9',
+        'ready-blue' : '#62BAFF',
+        'main-blue' : '#0085C9',
+        'finish-blue' : '#497BCA',
         },
       backgroundImage: theme => ({
         'custom-gradient': 'linear-gradient(180deg, #0085C9 0%, #FFF 100%)',
+        'custom-gradient2': 'linear-gradient(180deg, #FFF 0%, #0085C9 100%)',
       }),
 
       fontFamily : {
