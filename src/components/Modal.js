b75d9b12ddff62840d87f4c36a1b74e694a9f0c4
777Mino777 <petctusweet@naver.com>
2023-12-12 18:10:06 +0900

    modal
    modal 수정

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,7 @@
 const Modal = ({ title, children, onClose }) => {
     return (
       <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center">
-        <div className="border-8 rounded-lg bg-white shadow-lg">
+        <div className="border-4 rounded-lg bg-white shadow-lg">
           <div className="px-4 bg-custom-blue text-white flex justify-between items-center">
             <h2 className="text-lg font-semibold">{title}</h2>
             <button onClick={onClose} className="text-xl font-bold hover:text-black">×</button>
